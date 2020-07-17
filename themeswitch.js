ca4218a0303043b242c9d242b77670f95d550a78
Anatoliy Vyskub <anatoliy.vyskub@gmail.com>
2020-07-18 01:36:20 +0400

    +localStorage

--- themeswitch.js
+++ themeswitch.js
@@ -1,7 +1,17 @@
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
 
-themeButton.onclick = function () {
+  themeButton.onclick = function () {
   page.classList.toggle('white-theme');
   page.classList.toggle('dark-theme');
+  localStorage.setItem('theme', page.classList);
 }
+
+
+if (localStorage.getItem('theme') === 'page white-theme') {
+  page.classList.remove('dark-theme');
+  page.classList.add('white-theme');
+} else {
+  page.classList.add('dark-theme');
+  page.classList.remove('white-theme');
+};
